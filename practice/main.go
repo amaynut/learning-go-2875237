655f66360db86672a69b3f81e487c9a9b8f62296
Said Guerrab <said.guerrab@gmail.com>
2022-01-30 10:20:54 -0500

    convert string inputs to other types using strconv pkg

--- practice/main.go
+++ practice/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,4 +15,14 @@ func main() {
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered:", input)
 
+	fmt.Print("Enter a number: ")
+	myNumber, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(myNumber), 64)
+
+	// error handling
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("The number you entered is: ", aFloat)
 }
