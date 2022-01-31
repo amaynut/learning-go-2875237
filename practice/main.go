710b292913cdedd01a0e8d071d649c7af4b169fe
Said Guerrab <said.guerrab@gmail.com>
2022-01-31 08:51:23 -0500

    Reference values in array

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 2658
+	p := &anInt
+	fmt.Println("The memory address p is pointing to is: ", p)
+	fmt.Println("The value of p is: ", *p)
+
+	// change the original variable
+	anInt = 555
+	fmt.Println("The memory address p is pointing to is still: ", p)
+	fmt.Println("The value of p is now: ", *p)
+
+	// change the pointer variable
+	*p = 888
+	fmt.Println("The value of p is now: ", *p)
+	fmt.Println("The value of anInt is now: ", anInt)
+
 }
