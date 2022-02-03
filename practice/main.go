0d3330fb9772fa035bfec8a3030cb40e47f76aca
Said Guerrab <said.guerrab@gmail.com>
2022-02-03 08:07:48 -0500

    Evaluate expressions with switch statements

--- practice/main.go
+++ practice/main.go
@@ -7,7 +7,20 @@ import (
 )
 
 func main() {
+	// generate a random number between 1 and 7
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(7) + 1
 	fmt.Println("Day", dow)
+
+	// in golang, the break is the default behavior (to execute the next case we need fallthrough keyword
+	switch dow {
+	case 1:
+		print("It's Sunday")
+		// fallthrough
+	case 2:
+		print("It's Monday")
+		// fallthrough
+	default:
+		print("It's some other day")
+	}
 }
