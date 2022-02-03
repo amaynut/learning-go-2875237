3ce46843aa3fbe9299e3f267e32dd2cf1e7a9e0a
Said Guerrab <said.guerrab@gmail.com>
2022-02-03 07:49:52 -0500

    Program conditional logic

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	fmt.Println("************ Conditional logic ****************")
+	// get a number from the console
+	reader := bufio.NewReader(os.Stdin)
+	print("Enter a number: ")
+	input, _ := reader.ReadString('\n')
+	number, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		panic("The value is not a number")
+	}
+
+	// use an if else statement
+	var result string
+	if number < 0 {
+		result = "It's a negative number"
+	} else if number == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "It's a positive number"
+	}
+	println(result)
+
+	// declare a variable inside the if condition
+	if currentHour := time.Now().Hour(); currentHour < 7 {
+		fmt.Print("Good Night")
+	} else if currentHour >= 7 && currentHour < 12 {
+		print("Good morning")
+	} else if currentHour >= 12 && currentHour < 18 {
+		print("Good afternoon!")
+	} else {
+		print("Good evening")
+	}
+
 }
