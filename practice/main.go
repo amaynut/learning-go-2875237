71c4ac389dd12f358c7d4d64a3d3ccaadbd46dd4
Said Guerrab <said.guerrab@gmail.com>
2022-02-06 12:22:59 -0500

    Parse JSON-formatted text

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
+// Network request in Go!
 func main() {
-	fmt.Println("Network requests")
+	fmt.Println("*************** Network requests *************")
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response type: %T\n", response)
+
+	defer response.Body.Close()
+
+	bytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	print(string(bytes))
 }
