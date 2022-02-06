d5838113762e0bbd45944319c9df9fcd56d88de5
Said Guerrab <said.guerrab@gmail.com>
2022-02-06 10:15:37 -0500

    Define functions as methods of custom types

--- practice/main.go
+++ practice/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "Haw haw!"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Speak()
+	poodle.SpeakThreeTimes()
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+// Speak Type method (exported)
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+// SpeakThreeTimes is how to dog speaks loudly
+func (d Dog) SpeakThreeTimes() {
+	fmt.Println(strings.Repeat(d.Sound+" ", 3))
 }
